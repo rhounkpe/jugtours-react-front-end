9d4dee130c024b935e476c9ea3de1a9656c7bbcf
Rufin Hounkpe <rhounkpe@gmail.com>
2020-09-18 23:03:47 +0200

    Call Spring Boot API and display Group list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            groups: []
+        };
+
+    }
+
+    async componentDidMount() {
+        const response = await fetch('http://localhost:8080/api/groups');
+        const body = await response.json();
+        this.setState({
+            groups: body,
+            isLoading: false
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className="App-intro">
+                        <h2>JUG List</h2>
+                        {this.state.groups.map(group =>
+                            <div key={group.id}>
+                                {group.name}
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
